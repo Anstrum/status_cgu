83873b877e69154bee01de021def36c49b0944e8
Nako <Nanaka.kumiho@gmail.com>
2022-12-04 23:54:47 +0100

    adding script button

---
+++ index.js
@@ -0,0 +1,8 @@
+let icon = document.querySelector(".logo");
+let scrollButton = document.querySelector(".scroll-up");
+
+scrollButton.addEventListener("click", () => {
+    //scroll to the top
+})
+
+//detect the scroll to show the button
